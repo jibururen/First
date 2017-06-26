70e96a3ab0df9b00847edacc6b5789d5bf6754d0
jibururen <jiuzhangji@163.com>
2017-06-26 16:16:14 +0800

    make a flask app

---
+++ a.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world'
+
+if __name__ == '__main__':
+    app.run('192.168.1.1',9000)
